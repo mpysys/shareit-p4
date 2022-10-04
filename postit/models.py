013476591fcf8a112947b0c09adb276781ba8e49
mpysys <monnet.morgan@gmail.com>
2022-10-04 06:01:22 +0000

    add url and views for postit app

--- postit/models.py
+++ postit/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-#this model/class is for the id/content whn users will post
+# this model/class is for the id/content whn users will post
+
+
 class Postit(models.Model):
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
