19ba05a4be383ddd9ebfedb87f1f14a4d4f14f0f
mpysys <monnet.morgan@gmail.com>
2022-10-04 05:52:01 +0000

    Add postit app setup

---
+++ postit/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+#this model/class is for the id/content whn users will post
+class Postit(models.Model):
+    content = models.TextField(blank=True, null=True)
+    image = models.FileField(upload_to='images/', blank=True, null=True)
