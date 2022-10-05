3ec2335d1a241f118248d7beeedc67d1e681656a
mpysys <monnet.morgan@gmail.com>
2022-10-05 05:49:09 +0000

    Added user name in admin panel for objects

--- postit/admin.py
+++ postit/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Postit
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'user']
+    class Meta:
+        model = Postit
+admin.site.register(Postit, PostAdmin)
