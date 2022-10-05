600b74dcf90174a733e28aca84838040208264e7
mpysys <monnet.morgan@gmail.com>
2022-10-05 05:17:59 +0000

    Created superuser

--- postit/models.py
+++ postit/models.py
@@ -1,12 +1,17 @@
 import random
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+
+User = settings.AUTH_USER_MODEL
 
 # this model/class is for the id/content whn users will post
 
 
 class Postit(models.Model):
+    # Map to SQL Data
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # I want to delete everything from a user if user is deleted
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
