2a84c09103809892e7f289bdc702569249b7e585
mpysys <monnet.morgan@gmail.com>
2022-10-15 07:20:35 +0000

    Create PostLike class and add timestamps to posts

--- postit/models.py
+++ postit/models.py
@@ -6,15 +6,21 @@ from django.db import models
 
 User = settings.AUTH_USER_MODEL
 
+class PostLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey("Postit", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
 # this model/class is for the id/content whn users will post
 
 
 class Postit(models.Model):
     # Map to SQL Data
     user = models.ForeignKey(User, on_delete=models.CASCADE) # I want to delete everything from a user if user is deleted
+    likes = models.ManyToManyField(User, related_name='post_user', blank=True, through=PostLike)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
-
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
     class Meta:
         ordering = ['-id']
 
